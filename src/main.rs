844c356dcd9f4adb4692a5befcbff51405609d4a
Luizhnrs <Luizhnrsg@gmail.com>
2024-04-22 13:26:56 -0300

    Pendulum Structure Created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Pendulum {
+    //This is the angle of the pendulum
+    angle: f32,
+
+    angular_velocity: f32,
+    angular_acceleration: f32,
+
+    r:f32, //The length of the pendulum
+    m:f32, //The mass of the ball
+    g:f32, //The gravity
+}
+
