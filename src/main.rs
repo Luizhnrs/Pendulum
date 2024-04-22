7c257295bd9d88f550caf85a3577fd613db259eb
Luizhnrs <Luizhnrsg@gmail.com>
2024-04-22 13:47:10 -0300

    Vector Modules is implemented for now

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ fn main() {
 }
 
 struct Pendulum {
+    //This vector is the position of the pendulum
+    origin: vector::Vector,
+
+    //This vector is the position of the ball
+    position: vector::Vector,
+
     //This is the angle of the pendulum
-    angle: f32,
+    angle: f32, 
 
     angular_velocity: f32,
     angular_acceleration: f32,
@@ -13,4 +19,47 @@ struct Pendulum {
     m:f32, //The mass of the ball
     g:f32, //The gravity
 }
+
+impl Pendulum {
+    fn new(){
+
+    }
+
+    fn update(){
+
+    }
+
+    fn draw(){
+
+    }
+}
+
+mod vector {
+    use std::io::SeekFrom;
+
+
+    struct Vector {
+        pub x: f32,
+        pub y: f32,
+    }
+
+    impl Vector {
+        pub fn new(x:f32, y:f32) -> Vector {
+            Vector{ x, y }
+        }
+
+        pub fn add(&mut self, other: Vector) -> &Vector{
+            self.x += other.x;
+            self.y += other.y;
+            self
+         }
+
+        pub fn set(&mut self, x: f32, y:f32){ 
+            self.x = x;
+            self.y = y;
+        }
+    }
+
+}
+
 
